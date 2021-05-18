7d0663ec713a05a5ab59b1ed62cbc6c1b12e396a
gedunlap <g.e.dunlap90@gmail.com>
2021-05-18 18:32:15 -0400

    create new entry, datepicker broken

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,7 +1,30 @@
 import {useState} from "react"
 import {Link} from "react-router-dom"
+import DatePicker from "react-date-picker"
 
 function Index(props) {
+
+    const [newForm, setNewForm] = useState({
+        name: "",
+        title: "",
+        date: "",
+        entry: ""
+    })
+
+    const handleChange = (event) => {
+        setNewForm({...newForm, [event.target.name]: event.target.value})
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.createEntries(newForm)
+        setNewForm({
+            name: "",
+            title: "",
+            date: "",
+            entry: ""
+        })
+    }
 
     const loaded = () => {
         return props.entries.map((entry) => (
@@ -17,7 +40,41 @@ function Index(props) {
         return <h1>Loading...</h1>
     }
 
-    return props.entries ? loaded() : loading()
+    return (
+        <section>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text" 
+                    value={newForm.name}
+                    name="name"
+                    placeholder="Name"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text" 
+                    value={newForm.title}
+                    name="title"
+                    placeholder="Title"
+                    onChange={handleChange}
+                />
+                <DatePicker 
+                    onChange={handleChange}
+                    value={newForm.date}
+                    name="date"
+                />
+                <input 
+                    type="text"
+                    value={newForm.entry}
+                    name="entry"
+                    placeholder="Type Jornal Entry Here..."
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Create Entry" />
+            </form>
+            {props.entries ? loaded() : loading()}
+        </section>
+        
+    )
 
 }
 
